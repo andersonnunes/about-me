c00162792f546517fd5efafb9f579b9536717240
Anderson Nunes <anderson.nunes1988@gmail.com>
2022-05-26 03:40:14 -0300

    Step 04 - Exercise - EditText, DoneButton ClickHandler

--- app/src/main/java/com/example/android/aboutme/MainActivity.kt
+++ app/src/main/java/com/example/android/aboutme/MainActivity.kt
@@ -7,5 +7,8 @@ class MainActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // TODO (04) Add a click handler for the Done button to process the input and update the visibility of the views
+
     }
 }
