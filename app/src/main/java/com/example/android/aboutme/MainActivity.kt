2473954e074779aab75e3480f52c02e4483734d0
Anderson Nunes <anderson.nunes1988@gmail.com>
2022-05-26 03:54:59 -0300

    Step 04 - Solution - EditText, DoneButton ClickHandler

--- app/src/main/java/com/example/android/aboutme/MainActivity.kt
+++ app/src/main/java/com/example/android/aboutme/MainActivity.kt
@@ -1,6 +1,12 @@
 package com.example.android.aboutme
 
+import android.content.Context
 import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity: AppCompatActivity() {
@@ -8,7 +14,21 @@ class MainActivity: AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // TODO (04) Add a click handler for the Done button to process the input and update the visibility of the views
+        findViewById<Button>(R.id.done_button).setOnClickListener {
+            addNickname(it)
+        }
+    }
 
+    private fun addNickname(view: View) {
+        val ediText: EditText = findViewById(R.id.nickname_edit)
+        val nicknameTextView: TextView = findViewById(R.id.nickname_text)
+
+        nicknameTextView.text = ediText.text
+        ediText.visibility = View.GONE
+        view.visibility = View.GONE
+        nicknameTextView.visibility = View.VISIBLE
+
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view.windowToken, 0)
     }
 }
