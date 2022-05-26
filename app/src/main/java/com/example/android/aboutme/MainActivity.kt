fe543663aae65f85cc25cd64f528ae6f272ed6c7
Anderson Nunes <anderson.nunes1988@gmail.com>
2022-05-26 02:59:04 -0300

    Step 02 - Exercise - Add TextView, ImageView and Style

--- app/src/main/java/com/example/android/aboutme/MainActivity.kt
+++ app/src/main/java/com/example/android/aboutme/MainActivity.kt
@@ -3,4 +3,5 @@ package com.example.android.aboutme
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity(contentLayoutId: Int) : AppCompatActivity(contentLayoutId) {
+    // TODO (02) Connect the layout to the activity using setContentView()
 }
